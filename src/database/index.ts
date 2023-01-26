bf3214855225bd25c09c3601ed3a2511eb33227b
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2023-01-25 22:27:37 -0300

    feat: novo cluster mongo db atlas

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,6 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-    process.env.NODE_ENV === "production" 
-    ? mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xqea7mj.mongodb.net/?retryWrites=true&w=majority`)
-    : mongoose.connect('mongodb://localhost:27017/test') 
-
-    
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xqea7mj.mongodb.net/?retryWrites=true&w=majority`)
 
 export default mongoose;
